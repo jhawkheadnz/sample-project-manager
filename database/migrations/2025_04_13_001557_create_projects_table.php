33a47c2334aede868c9edba3510b9d0804979849
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2025-04-13 12:52:09 +1200

    Set up projects table

--- database/migrations/2025_04_13_001557_create_projects_table.php
+++ database/migrations/2025_04_13_001557_create_projects_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->dateTime("start_date");
+            $table->foreignIdFor(User::class);
+            $table->boolean("completed");
+            $table->dateTime("completed_date")->nullable();
             $table->timestamps();
         });
     }
