11b98a989793e2bc8403844a824cf240b14b5633
Joshua Hawkhead <joshua.hawkhead@gmail.com>
2025-04-13 13:14:00 +1200

    Set up tags table

--- database/migrations/2025_04_13_001856_create_tags_table.php
+++ database/migrations/2025_04_13_001856_create_tags_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->string("color");
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
