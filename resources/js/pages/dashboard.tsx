12e9e0e085fbaea647cbb089d594274733079455
gourmetdev <joshua@nzg.co.nz>
2025-04-18 21:18:02 +1200

    Updated dashboard - will be displaying recent projects

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
@@ -15,7 +16,14 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                
+                <Card>
+                    <CardHeader>
+                        <div>Recent Projects</div>
+                        <p className="text-gray-500 text-sm">Here are all the recently added projects...</p>
+                    </CardHeader>
+                    <CardContent>
+                    </CardContent>
+                </Card>
             </div>
         </AppLayout>
     );
